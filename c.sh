ef7a7ae3f34885e31c2bd2701141afd08c61e615
ebrahimbd <ebrahim16sust@gmail.com>
2022-09-23 01:28:38 +0600

    solve issu fixex #2

---
+++ c.sh
@@ -0,0 +1,16 @@
+
+echo "Sdfdsf"
+
+if [[ -f DroidSansMono.zip ]]
+then
+    echo "<file> exists on your filesystem."
+fi
+
+
+FILE=/DroidSansMono.zip
+  if test -f "$FILE"; then
+    echo "$FILE exists."
+    else
+        echo "sdjlfkdsfjdslkjlkjlkjlkjkljkjlkjkjlj"
+  fi
+
--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 install () {
   sudo apt install build-essential cmake
+  sudo pip install cmake
   sudo sudo cp .vimrc ~/
   echo "[-] Download fonts [-]"
   echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip"
+    if [[ -f DroidSansMono.zip ]]
+    then
+        echo " DroidSansMono.zip exists on your filesystem."
+        unzip DroidSansMono.zip -d ~/.fonts
+        fc-cache -fv
+    else
   wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip
   unzip DroidSansMono.zip -d ~/.fonts
   fc-cache -fv
-
+    fi
 }
 
 
